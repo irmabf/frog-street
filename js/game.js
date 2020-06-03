ac917bb54aa98856749df2c0266c8c1a24c6a925
Irma Blanco <irblancofdz@gmail.com>
2020-06-03 19:23:55 +0200

    feat: Game class

---
+++ js/game.js
@@ -0,0 +1,30 @@
+class Game {
+
+  constructor(ctx) {
+    this._ctx = ctx
+    this._intervalId = null
+
+    this._bg = new Background(this._ctx)
+
+    this._tick = 0
+  }
+
+  start() {
+    this._intervalId = setInterval(() => {
+      this._clear()
+      this._draw()
+    }, 1000 / 60)
+  }
+
+  _draw() {
+    if (this._tick++ > 10000) {
+      this._tick = 0
+    }
+
+    this._bg.draw()
+  }
+  _clear() {
+    this._ctx.clearRect(0, 0, this._ctx.canvas.width, this._ctx.canvas.height)
+  }
+
+}
